9ebd153eb4aa46a35396ac9bf80a07157d9a241b
Vasilii <mihalcukv552@gmail.com>
2024-09-11 13:10:32 +1100

    dimm gotovo

---
+++ main2.py
@@ -0,0 +1,53 @@
+from tkinter import *
+from tkinter import messagebox
+
+# Функция для перемещения бегемота
+def move(event):
+    # Определяем движение в зависимости от нажатой клавиши
+    if event.keysym == 'w':
+        canvas.move("hippo", 0, -10)  # Вверх
+    elif event.keysym == 's':
+        canvas.move("hippo", 0, 10)   # Вниз
+    elif event.keysym == 'a':
+        canvas.move("hippo", -10, 0)  # Влево
+    elif event.keysym == 'd':
+        canvas.move("hippo", 10, 0)   # Вправо
+    else:
+        messagebox.showinfo("Ошибка!", "Нажмите одну из предложенных клавиш: W, A, S, D")
+
+root = Tk()
+root.title("Мото-мото")
+root.geometry("500x500")
+
+canvas = Canvas(root, height=500, width=500)
+canvas.pack()
+
+# Рисуем бегемота (все части объединены с тегом "hippo")
+# Уши
+canvas.create_oval(170, 180, 205, 135, fill='black', outline='', tags="hippo")
+canvas.create_oval(290, 180, 325, 135, fill='black', outline='', tags="hippo")
+
+# Дыры в ушах
+canvas.create_oval(180, 170, 195, 145, fill='white', outline='', tags="hippo")
+canvas.create_oval(300, 170, 315, 145, fill='white', outline='', tags="hippo")
+
+# Тело
+canvas.create_oval(100, 250, 400, 400, fill='grey70', outline='', tags="hippo")
+canvas.create_oval(150, 150, 350, 300, fill='grey70', outline='', tags="hippo")
+
+# Глаза
+canvas.create_oval(200, 230, 235, 195, fill='white', tags="hippo")
+canvas.create_oval(260, 230, 295, 195, fill='white', tags="hippo")
+
+# Зрачки
+canvas.create_oval(210, 225, 225, 210, fill='black', tags="hippo")
+canvas.create_oval(270, 225, 285, 210, fill='black', tags="hippo")
+
+# Ноздри
+canvas.create_oval(190, 360, 205, 325, fill='black', tags="hippo")
+canvas.create_oval(280, 360, 295, 325, fill='black', tags="hippo")
+
+# Привязываем обработку событий клавиатуры к окну
+root.bind('<Key>', move)
+
+root.mainloop()
